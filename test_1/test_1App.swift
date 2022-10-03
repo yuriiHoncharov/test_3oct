15faf9a3f078b333b1ce54abaf17cf727894acf7
yuriiHoncharov <y.honcharov@lampalampa.net>
2022-10-03 10:23:18 +0300

    first commit

---
+++ test_1/test_1App.swift
@@ -0,0 +1,17 @@
+//
+//  test_1App.swift
+//  test_1
+//
+//  Created by Yurii Honcharov on 03.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct test_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
